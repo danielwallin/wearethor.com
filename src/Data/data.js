ecd39edd325b23723d63a99775f1894fef2f76a7
Daniel <daniel@danielwallin.com>
2020-05-20 22:34:35 +0200

    Updated text, added image and change preview image

--- src/Data/data.js
+++ src/Data/data.js
@@ -9,7 +9,7 @@ export const data = {
     {
       title: 'This is us',
       text:
-        'Once upon a time, we lived in an art collective in the high jungles of Peru. We lived a spectacular life, got married by the most mysterious shaman, and traveled through many different dimensions. This saga began when we made the decision to go on a mythic adventure, just the two us, and painted our first mural in a hostel in small-town Tarapoto, Peru.  <br /><br />Love/ Mr and Mrs THOR',
+        'Once upon a time, we lived in an art collective in the high jungles of Peru. We lived a spectacular life, got married by the most mysterious shaman, and traveled through many different dimensions. This saga began when we made the decision to go on a mythic adventure, just the two us, and painted our first mural in a hostel in the small town of Tarapoto, Peru.',
       subtitle: 'One of the last murals we did in South America - May 2019',
       img: '3.jpg',
       imgsize: '95vw',
@@ -19,13 +19,13 @@ export const data = {
     {
       title: 'WHAT WE DO',
       text:
-        'We capture magical moments of happiness between humans and animals, as if someone in the painting just told the most hilarious joke to the other. Our aim is to make the observer laugh or at the very least feel good and smile inside. We paint the world a happier place with brushes, sponges, fingers and most of all; good energy!',
+        'We capture magical moments of happiness between humans and animals, as if someone in the painting just told the most hilarious joke to the other.<br /><br /> Our aim is to make the observer laugh or at the very least feel good and smile inside. We paint the world a happier place with brushes, sponges, fingers and most of all: <br />good energy!',
     },
     {
       title: '"Joyce"',
       type: 'work',
       text:
-        'While living in Peru we were deeply inspired by the traditions of the natives. Joyce is a happy native Shipibo girl from the jungle of Peru, she is immersed in the old ancient icaros patterns that are sung by the initiated during ayahuasca ceremonies.',
+        'While living in Peru we were deeply inspired by the traditions of the natives. Joyce is a happy native Shipibo girl from the Peruvian jungle. She is immersed in the old ancient icaros patterns that are sung by the initiated ones during ayahuasca ceremonies.',
       subtitle: 'Mancora, Peru - January 2019',
       img: '5.jpg',
       imgpreview: '5_prev.jpg',
@@ -48,63 +48,19 @@ export const data = {
       imgpreview: '9_prev.jpg',
       imgalt: 'The Joke',
       type: 'work',
-      text: 'So imagine this... <br /><br /> his cheeky maccaw just told the most hilarious joke to the man and then turned around proudly to us as to say -"How funny was that?"',
+      text: 'So imagine this... <br /><br /> His cheeky macaw just told the most hilarious joke to the man,<br /> turning around proudly to us, as if to say<br/> "How funny was that, huh?"',
       subtitle: 'Mancora, Peru - January 2019',
     },
-    // {
-    //   title: '"MOMPICHE SPIRIT"',
-    //   type: 'work',
-    //   text:
-    //     'With the jaguar, toucan, roses, and patterns, we wanted to capture the Ecuadorian spirit in Mompiche through the local flora and fauna. Combined they symbolize showmanship, passion, love, confidence, and focus.',
-    //   img: '4.png',
-    //   imgpreview: '4_prev.png',
-    //   imgsize: '95vw',
-    //   imgalt: 'Mompiche spirit',
-    //   subtitle: 'MOMPICHE, Equador - February 2019',
-    // },
-    // {
-    //   title: '"WAYUU WOMAN"',
-    //   type: 'work',
-    //   text:
-    //     'This is a woman with traditional features from one of the few intact tribes left in Colombia, the Wayuu. They are deeply spiritual and connected to nature. We wanted to give tribute to their traditions by painting traditional patterns and fine details in the straw hat and earrings.',
-    //   layout: 'left',
-    //   img: '13.jpg',
-    //   imgpreview: '13_prev.jpg',
-    //   imgalt: 'Mompiche spirit',
-    //   imgsize: '80vw',
-    //   imgclass: 'mtb-3',
-    //   imgtext: '<div class="work-img-subtitle inset">Cartagena, Colombia April 2019</div>',
-    // },
     {
       title: '"CARTALEYNA"',
       type: 'work',
       text:
-        'We wanted to capture a modern image of Cartagena and picked our favourite waitress, Leyna, from our favourite restaurant in Cartagena, to model for us. All of the colorful birds in Cartagena inspired us to paint these two lovebirds. The pattern is typical pre-Colombian.',
+        'We wanted to capture a modern image of Cartagena and picked our favourite waitress, Leyna, from our favourite restaurant in Cartagena, to model for us. All of the colorful birds in Cartagena inspired us to paint these two lovebirds. The pattern is typical pre-Colombian style.',
       img: '15.jpg',
       imgpreview: '15_prev.jpg',
       imgalt: 'Cartaleyna',
       subtitle: 'Cartagena, Colombia - April 2019',
     },
-    // {
-    //   title: '"TOVNER"',
-    //   type: 'work',
-    //   text:
-    //     'Being back in Sweden we felt a strong connection to Nordic mythology and choose to paint Tovner, one of the lynx that drags goddess Frejas carriage. The lynx symbolizes clarity, wisdom and magic. The sunset in the forest highlights the heather flowers, making it a place with many mysteries. The white pattern is Yggdrasil, the Nordic tree of life.',
-    //   img: '17.jpg',
-    //   imgalt: 'Tovner',
-    //   imgpreview: '17_prev.jpg',
-    //   subtitle: 'Stockholm, Sweden - July 2019',
-    // },
-    // {
-    //   title: '"FREJAS FALKHAMN"',
-    //   type: 'work',
-    //   text:
-    //     '"Frejas falcon harbour" also originates from Nordic mythology, it&#39;s a spiritual feather coat that magically transforms the one who wears it into a falcon. A powerful being of wisdom, vision, and protection. The peach-colored roses are a symbol of honesty and gratitude. The white pole vault symbolizes protection against obstacles that can cause a person to lose their path.',
-    //   img: '19.jpg',
-    //   imgalt: 'Frejas falkhamn',
-    //   imgpreview: '19_prev.jpg',
-    //   subtitle: 'Stockholm, Sweden - August 2019',
-    // },
     {
       type: 'grid',
       hideNav: true,
@@ -119,7 +75,7 @@ export const data = {
     },
 
     {
-      title: 'SHENANIGANS',
+      title: '"SHENANIGANS"',
       type: 'work',
       text: 'The best friendships are built on a solid foundation of silliness, shenanigans and general misbehaviour.',
       img: 'Shenanigans small.jpg',
@@ -128,16 +84,16 @@ export const data = {
     },
 
     {
-      title: 'KIDDING',
+      title: '"KIDDING"',
       type: 'work',
-      text: "Inside every older person is a younger person wondering what the hell happened. Don't grow up, it's a trap! ",
+      text: "Inside every older person is a younger person wondering what the hell happened.<br /> Don't grow up, it's a trap! ",
       img: 'Kidding small.jpg',
       imgpreview: 'Kidding small_prev.jpg',
       subtitle: '150x120 cm, acrylics on canvas.',
     },
 
     {
-      title: 'OLD FRIENDS',
+      title: '"OLD FRIENDS"',
       type: 'work',
       text: "When you've known each other for a long time,  you can laugh at anything together. Even dirty jokes.",
       img: 'Old Friends small.jpg',
@@ -146,88 +102,75 @@ export const data = {
     },
 
     {
-      // title: 'HOMEBOYS',
       type: 'work',
       layout: 'right no-margin',
       imgsize: '50vw',
-      imgtext: '<h2 class="work-title">HOMEBOYS</h2><br />Friends come in all shapes and sizes, you are never alone.<br /><br /> 80x120 cm, acrylics on canvas.',
+      imgtext:
+        '<h2 class="work-title">HOMEBOYS</h2><br />Friends come in all shapes and sizes;<br /> you are never alone.<br /><br /><div class="work-img-subtitle">80x120 cm, acrylics on canvas.</div>',
       imgclass: 'm-6',
-      //text: 'Friends come in all shapes and sizes, you are never alone.',
       img: 'Homeboys small.jpg',
       imgpreview: 'Homeboys small_prev.jpg',
-      //subtitle: '80x120 cm, acrylics on canvas.',
     },
-
     {
-      title: 'BESTIES',
+      title: '"BESTIES"',
       type: 'work',
       text: 'Best friends make good times better and hard times easier',
       img: 'Besties small.jpg',
       imgpreview: 'Besties small_prev.jpg',
       subtitle: '150x120 cm, acrylics on canvas.',
     },
-
     {
-      title: 'HORSING AROUND',
+      title: '"HORSING AROUND"',
       type: 'work',
       text: "If we don't have fun, then what is the fucking point?",
       img: 'Horsing around small.jpg',
       imgpreview: 'Horsing around small_prev.jpg',
       subtitle: '120x80 cm, acrylics on canvas.',
     },
-
     {
-      title: 'ZMILE',
+      title: '"ZMILE"',
       type: 'work',
-      text: 'There is always a reason to smile. Find it!',
+      text: 'There is always a reason to smile.<br /> Find it!',
       img: 'Zmile small.jpg',
       imgpreview: 'Zmile small_prev.jpg',
       subtitle: '120x80 cm, acrylics on canvas.',
     },
-
     {
-      title: 'GIGGLES',
+      title: '"GIGGLES"',
       type: 'work',
       text: 'Worry less, giggle more!',
       img: 'Giggles small.jpg',
       imgpreview: 'Giggles small_prev.jpg',
       subtitle: '150x120 cm, acrylics on canvas.',
     },
-
     {
-      // title: 'MISS PIGGY',
-      // subtitle: '100x140 cm, acrylics on canvas.',
-      // imgsize: '95vw',
       layout: 'left no-margin',
       imgclass: 'm-6',
-      imgtext: '<h2 class="work-title">MISS PIGGY</h2><br />When in doubt, pig out! <br /><br /> 100x140 cm, acrylics on canvas.',
+      imgtext: '<h2 class="work-title">MISS PIGGY</h2><br />When in doubt, pig out! <br /><br /><div class="work-img-subtitle">100x140 cm, acrylics on canvas.</div>',
       type: 'work',
       imgsize: '50vw',
-      // text: 'When in doubt, pig out!',
       img: 'Miss piggy small.jpg',
       imgpreview: 'Miss piggy small_prev.jpg',
     },
 
     {
-      title: 'HOMEGIRLS',
+      title: '"HOMEGIRLS"',
       type: 'work',
       text: 'Behind every happy woman is her dope homegirl.',
       img: 'Homegirls small1.jpg',
       imgpreview: 'Homegirls small1_prev.jpg',
       subtitle: '120x80 cm, acrylics on canvas.',
     },
-
     {
-      title: 'THOR FAMILY PORTRAIT',
+      title: '"THOR FAMILY PORTRAIT"',
       type: 'work',
       text: 'The key to happiness is dogs. Our puppy Lucy is a superstar!',
       img: 'Thor family small1.jpg',
       imgpreview: 'Thor family small1_prev.jpg',
       subtitle: '91x61 cm. acrylics on canvas.',
     },
-
     {
-      title: 'OTTER JOY',
+      title: '"OTTER JOY"',
       type: 'work',
       text: 'A commissioned piece to a otterly happy Lauren! ',
       img: 'Otter joy small.jpg',
@@ -235,36 +178,16 @@ export const data = {
       subtitle: '120x80 cm, acrylics on canvas. SOLD',
     },
     {
-      title: 'HAPPINESS',
+      title: '"HAPPINESS"',
       type: 'work',
+      img: 'Happiness small.jpg',
       text: 'We had the honor to create a mural in the incredible gallery aRtCastle in Kristianstad, Sweden during easter 2020.',
     },
-    // {
-    //   title: 'NOT YOUR <br /> ORDINARY CANVAS',
-    //   type: 'work mix',
-    //   text:
-    //     'We were given the opportunity to put our art on drums and said yes with no doubt. Once again we gave tribute to the Peruvian Shipibo traditions with it&#39;s traditional icaros patterns and the first four archetypes from the Q&#39;eroindians medicine wheel.',
-    //   grid: ['grid_3/3_1.jpg', 'grid_3/3_2.jpg', 'grid_3/3_3.jpg', 'grid_3/3_4.jpg', 'grid_3/3_5.jpg', 'grid_3/3_6.jpg'],
-    //   subtitle: 'Stockholm Sweden, September 2019',
-    // },
-
-    // {
-    //   type: 'work',
-    //   layout: 'right no-margin',
-    //   imgtext:
-    //     '<div class="work-img-subtitle">STOCKHOLM, SWEDEN <br /> MAY 2019</div>At our home in Sweden, outside the window, we saw this charming gathering and made our first oil painting together. <br /><br /> Oil on canvas 20 x 30 cm',
-    //   imgclass: 'm-6',
-    //   img: 'wedo.jpg',
-    //   imgalt: 'wedo',
-    //   imgpreview: 'wedo_prev.jpg',
-    //   imgsize: '50vw',
-    // },
     {
       title: 'Contact',
       id: 'contact',
       hideNav: true,
-      text:
-        '<a href="mailto:info@wearethor.com">info@wearethor.com</a> <br /> <a href="tel:+46 738 41 14 93">+46 738 41 14 93</a><br/><a href="wearethor.com">wearethor.com</a><br /><br />Follow our mythic adventure!',
+      text: '<a href="mailto:info@wearethor.com">info@wearethor.com</a> <br /> <a href="tel:+46 738 41 14 93">+46 738 41 14 93</a><br/><br /><br />Follow our mythic adventure!',
       links: [
         {
           icon: 'img/social/instagram.png',
@@ -273,19 +196,7 @@ export const data = {
         {
           icon: 'img/social/facebook.jpg',
           url: 'https://www.facebook.com/we-are-THOR-102010497844214/?modal=admin_todo_tour',
-        },
-        // {
-        //   icon: 'img/social/linkedin.png',
-        //   url: 'https://www.linkedin.com/company/we-are-thor/?viewAsMember=true',
-        // },
-        // {
-        //   icon: 'img/social/pinterest.png',
-        //   url: 'https://www.pinterest.se/carolinem50530597',
-        // },
-        // {
-        //   icon: 'img/social/etsy.png',
-        //   url: 'https://www.etsy.com/shop/WeAreTHOR',
-        // },
+        }
       ],
     },
   ],
